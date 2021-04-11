74663e9507e76a72f9fe8b251e1d572c9c46a4e8
QPThree <youngqp3@gmail.com>
2021-04-11 08:32:31 -0700

    added second if block to handle meal greeting

--- js/dynamiccontent.js
+++ js/dynamiccontent.js
@@ -6,19 +6,26 @@ var greeting;
 var meal;
 var ad;
 
+
 //determins greeting
 if (hourNow > 18) {
     greeting='Good Evening, '+userName;
-    meal = 0;
 } else if (hourNow > 12){
     greeting = 'Good Afternoon,' +userName;
 } else if (hourNow > 0){
     greeting = 'Good Morning, ' +userName;
-    meal = 1;
 } else {
     greeting = 'Welcome,'  +userName;
+}
+
+if (hourNow > 16){
+    meal = 0;
+} else if (hourNow > 11){
+    meal = 1;
+} else if (hourNow > 4){
     meal = 2;
 }
+
 
 //switch will 1. find what time it is, 2. calculate which menu would be adequate for that time, and 3. return
 // a printed ad for that menu
